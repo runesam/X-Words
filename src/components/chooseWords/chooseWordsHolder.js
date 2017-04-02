4555997237ebb5a92d28de22a15624679b13f7f3
rapkid <ahmed.badw@gmail.com>
2017-04-02 09:37:21 +0300

    choose words enhancements

--- src/components/chooseWords/chooseWordsHolder.js
+++ src/components/chooseWords/chooseWordsHolder.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  // Text,
+  Text,
   View,
   // Image,
   // Alert,
   ListView,
+  Alert,
   // ScrollView,
   Dimensions,
   Animated,
-  PanResponder
   // LayoutAnimation
 } from 'react-native';
 
@@ -18,6 +18,7 @@ import {
   Button,
   //  Spinner,
 } from '../common/';
+import generalUtils from '../../utils/generalUtils';
 import OneWord from './components/oneWord';
 
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
@@ -26,23 +27,38 @@ const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 // const onScroll = Animated.event([{ nativeEvent: { contentOffset: { x: xOffset } } }]);
 
 class ChooseWordsHolder extends Component {
+
   state = {
-    dataSource: ds.cloneWithRows([['row 1', 'row 2'], ['row 1', 'row 2'], ['row 1', 'row 2'], ['row 1', 'row 2'], ['row 1', 'row 2'], ['row 1', 'row 2']]),
+    dataSource: ds.cloneWithRows([['English', 'turkish', 5]]),
     marga: new Animated.Value(0),
+    rows: [0],
     temper: -1 * (Dimensions.get('window').width - 50),
     stepper: Dimensions.get('window').width - 40,
-    currentX: null
+    currentX: null,
+    level: 'orta',
+    not: [],
+    memberId: 4,
+    interestsNumber: 8,
+    getLink: 'get_new_words',
+    canGet: true
   };
   componentWillMount() {
-    console.log(654);
-    this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: this.test,
-      onMoveShouldSetPanResponder: this.test,
-      onPanResponderGrant: this.test,
-      onPanResponderMove: this.test,
-      onPanResponderRelease: this.test,
-      onPanResponderTerminate: this.test,
-    });
+    const apiData = {};
+    apiData.memberId = this.state.memberId;
+    apiData.not = this.state.not;
+    generalUtils.getMoreWords(this.state.getLink, apiData)
+    .then(data => {
+      this.setState({
+      dataSource: ds.cloneWithRows(data),
+      rows: data
+     });
+      for (var i = 0; i < data.length; i++) {
+        this.setState({
+          not: this.state.not.concat([data[i].word_id]),
+         });
+      }
+    })
+    .catch(reason => console.log(reason));
   }
   onPressMe() {
     Animated.timing(
@@ -53,94 +69,89 @@ class ChooseWordsHolder extends Component {
       temper: this.state.temper - this.state.stepper
     });
   }
-  goLeft() {
-    Animated.timing(
-      this.state.marga,
-      { toValue: this.state.temper }
-    ).start();
-    this.setState({
-      temper: this.state.temper - this.state.stepper
-    });
-  }
-  goRight() {
-    Animated.timing(
-      this.state.temper,
-      { toValue: this.state.marga }
-    ).start();
-    this.setState({
-      temper: this.state.temper - this.state.stepper
-    });
-  }
-  start(event) {
-    this.setState({ current: Math.floor(event.nativeEvent.pageX) });
-    console.log(Math.floor(event.nativeEvent.pageX));
-    return true;
-  }
-  end(event) {
-    if (this.state.current < Math.floor(event.nativeEvent.pageX)) {
-      this.goLeft();
-      console.log('go Left');
-    } else {
-      this.goLeft();
-      console.log('go Right');
+  getMore(){
+    console.log('hello');
+    const apiData = {};
+    apiData.memberId = this.state.memberId;
+    apiData.not = this.state.not;
+    generalUtils.getMoreWords(this.state.getLink, apiData)
+    .then(data => {
+      if(data.none){
+        Alert.alert('end');
+        this.setState({
+          canGet: false,
+        });
+    }else{
+      this.setState({
+        rows: this.state.rows.concat(data)
+      });
+      this.setState({
+        dataSource: ds.cloneWithRows(this.state.rows),
+        canGet: true
+      });
+      for (var i = 0; i < data.length; i++) {
+        this.setState({
+          not: this.state.not.concat([data[i].word_id]),
+         });
+      }
     }
-    return true;
+    })
+    .catch(reason => console.log(reason));
+  }
+  handleScroll(event){
+    const width = this.state.not.length;
+    const current = event.nativeEvent.contentOffset.x / Dimensions.get('window').width;
+    const ah = width - current;
+    console.log('width' + ah);
+    if (ah == 3 && this.state.canGet) {
+      this.setState({ canGet: false });
+      this.getMore();
+    }
   }
   ComponentDidMount() {
 
   }
   renderRow() {
-    return (data) => <OneWord data={data} />;
+    return (data, sectionID, rowID) => <OneWord data={data} key={rowID} lang={this.props.lang} choosed={0} left={10} />;
   }
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.swipContainer}>
         <ListView
           horizontal
-          style={styles.swipContainer}
+          pagingEnabled
+           onScroll={this.handleScroll.bind(this)}
           showsHorizontalScrollIndicator={false}
           dataSource={this.state.dataSource}
           renderRow={this.renderRow()}
-          pagingEnabled
         />
+      </View>
         <View style={styles.downPart}>
-          <Button
-            text={this.props.lang.title.start_test}
-            style={styles.SignUpButton}
-            textStyle={styles.SignUpButtonText}
-            onPressMe={this.onPressMe.bind(this)}
-          />
+          <View style={styles.downpart1}>
+            <Text style={styles.text}>{this.props.lang.text.custom}{this.state.level}, {this.state.interestsNumber}{this.props.lang.text.topic}</Text>
+          </View>
+          <View style={styles.downpart2}>
+            <View style={{ flex: 1 }}></View>
+            <View style={styles.buttonHolder}>
+              <Button
+                text={this.props.lang.text.change}
+                style={styles.SignUpButton}
+                textStyle={styles.SignUpButtonText}
+                onPressMe={this.onPressMe.bind(this)}
+              />
+            </View>
+            <View style={{ flex: 1 }}></View>
+          </View>
+          <View style={styles.downpart3}></View>
         </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  itemsContainer: {
-  },
-  switcher: {
-    flex: 3,
-    backgroundColor: 'white',
-    alignItems: 'stretch',
-    marginTop: 50,
-  },
-  switcherPadding: {
-    flex: 1,
-    marginTop: 30
-  },
-  wrapper: {
-  },
-  slide1: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    flexDirection: 'column'
-  },
   text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
+    color: 'white'
   },
   container: {
     flex: 1,
@@ -148,11 +159,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#00cccc'
   },
   swipContainer: {
-    flex: 4,
+    flex: Dimensions.get('window').height - 100,
   },
   downPart: {
-    flex: 0.8,
+    height: 100,
+    flexDirection: 'column',
     backgroundColor: '#00cccc'
+  },
+  downpart1: {
+    flex: 1.5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  downpart2: {
+    flex: 2,
+    flexDirection: 'row'
+  },
+  downpart3: {
+    flex: 0.5
+  },
+  SignUpButton: {
+    borderRadius: 45,
+  },
+  SignUpButtonText: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '300',
+    paddingTop: 12,
+    paddingBottom: 12
+  },
+  buttonHolder: {
+    flex: 1,
+    justifyContent: 'center',
+    borderColor: 'white',
+    borderRadius: 40,
+    borderWidth: 1,
+    padding: 5
   }
 });
 module.exports = ChooseWordsHolder;
--- src/components/chooseWords/components/oneWord.js
+++ src/components/chooseWords/components/oneWord.js
@@ -13,7 +13,7 @@ import {
   // TouchableWithoutFeedback
 } from 'react-native';
 // import renderIf from 'render-if';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/simplelineicons';
 import {
   // Button,
   // CardSection,
@@ -31,18 +31,24 @@ class OneWord extends Component {
 
   }
   componentWillMount() {
-
   }
   onPressMe() {
 
   }
   ComponentDidUpdate() {
-
   }
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text>{this.props.data[0]}</Text>
+        <View style={styles.part1}>
+          <Text style={styles.headLine}>{this.props.lang.text.choosed} {this.props.choosed} {this.props.lang.text.left} {this.props.left}</Text>
+        </View>
+        <View style={styles.part2}>
+          <Text style={styles.wordEnglish}>{this.props.data.english}</Text>
+          <Text style={styles.wordTurkish}>{this.props.data.turkish}</Text>
+        </View>
+        <View style={styles.part3}></View>
+        <View style={styles.part4}></View>
       </View>
     );
   }
@@ -50,13 +56,43 @@ class OneWord extends Component {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    width: Dimensions.get('window').width - 60,
+    width: Dimensions.get('window').width - 40,
     marginLeft: 20,
+    marginRight: 20,
     marginTop: 20,
+    alignSelf: 'stretch',
     backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 40
-  }
+    borderRadius: 5,
+    padding: 30,
+    flexDirection: 'column'
+  },
+  part1: {
+  flex: 1,
+  alignItems: 'center'
+  },
+  headLine: {
+  fontWeight: '600'
+  },
+  wordEnglish: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  wordTurkish: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#666666'
+  },
+  part2: {
+  flex: 3,
+  alignItems: 'center'
+  },
+  part3: {
+  flex: 2,
+  },
+  part4: {
+  flex: 3,
+  },
 });
 
 module.exports = OneWord;
