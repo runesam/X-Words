28578bce88f0d6bfa8ea8e81a031b8823393e0b3
Sam Ewdala <same7.hamada@gmail.com>
2017-04-08 19:16:08 +0300

    fix sound changing accent - enhance singlepage file importing - add ios props - get info library added

---
+++ src/components/singlePages/index.js
@@ -0,0 +1,7 @@
+export * from './chooseWords';
+export * from './confirmWords';
+export * from './home';
+export * from './Interests';
+export * from './Levels';
+export * from './Slider';
+export * from './SignUp';
