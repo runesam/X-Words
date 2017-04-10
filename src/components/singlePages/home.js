e6c6f0e6453e6a465e945298afee58c8854c9d0e
rapkid <ahmed.badw@gmail.com>
2017-04-10 13:25:14 +0300

    commet start today

--- src/components/singlePages/home.js
+++ src/components/singlePages/home.js
@@ -27,8 +27,7 @@ class HomePageHolder extends Component {
   componentWillMount() {
     const date = new Date();
     const newDate = parseInt(date.toLocaleDateString('en-GB').split('/').join(''), 10);
-    generalUtils.storageGetAllItems();
-    //generalUtils.storageSetItem('todaywords', null);
+    // generalUtils.storageSetItem('todaywords', null);
     generalUtils.storageSetItem('status', 'finished');
     if (this.props.replaceColor) {
       this.props.replaceColor('white');
